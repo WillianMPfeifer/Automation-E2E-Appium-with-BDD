2b269648be4fb313068cacfe23aa5b004bdc6716
DigiWillian <willian.pfeifer@digifred.com.br>
2025-11-11 08:52:25 -0300

    Fix: Traduzido para inglês funções comuns| E2EAPP-001

--- tests/pages/common_functions.py
+++ tests/pages/common_functions.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-# Pesquisar esses imports depois
 from tests.config.settings import Settings
 from datetime import datetime
 
@@ -11,33 +10,33 @@ class CommonFunctions:
         self.driver = driver
         self.wait = WebDriverWait(driver, Settings.EXPLICIT_WAIT)
     
-    def encontrar_elemento(self, locator, timeout=None):
+    def find_element(self, locator, timeout=None):
         wait_time = timeout if timeout else Settings.EXPLICIT_WAIT
         wait = WebDriverWait(self.driver, wait_time)
         return wait.until(EC.presence_of_element_located(locator))
     
-    def encontrar_elementos(self, locator, timeout=None):
+    def find_elements(self, locator, timeout=None):
         wait_time = timeout if timeout else Settings.EXPLICIT_WAIT
         wait = WebDriverWait(self.driver, wait_time)
         return wait.until(EC.presence_of_all_elements_located(locator))
     
-    def clicar(self, locator, timeout=None):
+    def click(self, locator, timeout=None):
         wait_time = timeout if timeout else Settings.EXPLICIT_WAIT
         wait = WebDriverWait(self.driver, wait_time)
         element = wait.until(EC.element_to_be_clickable(locator))
         element.click()
     
-    def digitar(self, locator, text, clear_first=True):
-        element = self.encontrar_elemento(locator)
+    def send_keys(self, locator, text, clear_first=True):
+        element = self.find_element(locator)
         if clear_first:
             element.clear()
         element.send_keys(text)
     
-    def obter_texto(self, locator):
-        element = self.encontrar_elemento(locator)
+    def get_text(self, locator):
+        element = self.find_element(locator)
         return element.text
     
-    def elemento_eh_visivel(self, locator, timeout=10):
+    def is_element_visible(self, locator, timeout=10):
         try:
             WebDriverWait(self.driver, timeout).until(
                 EC.visibility_of_element_located(locator)
@@ -46,7 +45,7 @@ class CommonFunctions:
         except TimeoutException:
             return False
     
-    def elemento_eh_presente(self, locator, timeout=5):
+    def is_element_present(self, locator, timeout=5):
         try:
             WebDriverWait(self.driver, timeout).until(
                 EC.presence_of_element_located(locator)
@@ -55,7 +54,7 @@ class CommonFunctions:
         except TimeoutException:
             return False
     
-    def aguarda_elemento_desaparecer(self, locator, timeout=10):
+    def wait_for_element_to_disappear(self, locator, timeout=10):
         try:
             WebDriverWait(self.driver, timeout).until(
                 EC.invisibility_of_element_located(locator)
@@ -64,33 +63,33 @@ class CommonFunctions:
         except TimeoutException:
             return False
     
-    def rolar_ate_elemento(self, locator):
-        element = self.encontrar_elemento(locator)
+    def scroll_to_element(self, locator):
+        element = self.find_element(locator)
         self.driver.execute_script("mobile: scrollToElement", {"elementId": element.id})
     
-    def deslizar_para_cima(self, duration=800):
+    def swipe_up(self, duration=800):
         size = self.driver.get_window_size()
         start_x = size['width'] // 2
         start_y = size['height'] * 0.8
         end_y = size['height'] * 0.2
         self.driver.swipe(start_x, start_y, start_x, end_y, duration)
     
-    def deslizar_para_baixo(self, duration=800):
+    def swipe_down(self, duration=800):
         size = self.driver.get_window_size()
         start_x = size['width'] // 2
         start_y = size['height'] * 0.2
         end_y = size['height'] * 0.8
         self.driver.swipe(start_x, start_y, start_x, end_y, duration)
     
-    def capturar_screenshot(self, name):
+    def capture_screenshot(self, name):
         Settings.ensure_directories()
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        nome_arquivo = f"{name}_{timestamp}.png"
-        caminho_arquivo = Settings.SCREENSHOTS_DIR / nome_arquivo
-        self.driver.save_screenshot(str(caminho_arquivo))
-        return str(caminho_arquivo)
+        file_name = f"{name}_{timestamp}.png"
+        file_path = Settings.SCREENSHOTS_DIR / file_name
+        self.driver.save_screenshot(str(file_path))
+        return str(file_path)
     
-    def esconder_teclado(self):
+    def hide_keyboard(self):
         try:
             self.driver.hide_keyboard()
         except:
